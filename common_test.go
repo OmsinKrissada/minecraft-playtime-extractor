b0168d85519815b12b6695a59c220718c439b868
Krissada Singhakachain <omsin_47@hotmail.com>
2026-07-19 23:58:38 +0700

    fix: updated error handling changes into unit test

--- common_test.go
+++ common_test.go
@@ -14,7 +14,8 @@ func TestGetWorldRunTime(t *testing.T) {
 
 func TestGetPlaytime(t *testing.T) {
 	config.WorldDir = "test_server_dir/world"
-	pt := getAllPlaytime()
+	pt, err := getAllPlaytime()
+	assert.NoError(t, err)
 	expectedResult := map[string]int{"0e3715e0-e902-4738-ba8a-f58f70c23e64": 49989, "2a2a2c18-c1e8-4ca6-b50e-2c6c7d509659": 11063603, "6bce8c3c-7c91-4993-865f-e9d28d557d1f": 203690, "d89b17bc-00d8-4b33-ba3a-28f18a58cb51": 21727052}
 	assert.EqualValues(t, expectedResult, pt)
 }
