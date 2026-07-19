de54915cb345534c53fd764fc523324966a76d24
Krissada Singhakachain <omsin_47@hotmail.com>
2026-07-19 22:57:31 +0700

    fix: use 6 min as active time to avoid false offline

--- website/src/assets/utils.ts
+++ website/src/assets/utils.ts
@@ -19,7 +19,7 @@ export function timeAgo(iso: string, forceRelative = false) {
 }
 
 export function isOnline(iso: string) {
-  return Date.now() - new Date(iso).valueOf() < 5 * 60 * 1000
+  return Date.now() - new Date(iso).valueOf() < 6 * 60 * 1000
 }
 
 export function fmtValue(h: number) {
@@ -31,5 +31,5 @@ export function fmtUnit(h: number) {
 }
 
 export function avatarUrl(uuid: string) {
-  return `https://mc-heads.net/avatar/${uuid}/8`
+  return `https://mc-heads.net/avatar/${uuid}`
 }
